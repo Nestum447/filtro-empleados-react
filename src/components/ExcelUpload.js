b145e5cc6030123aed2c29e5490ba53dbf449ec2
Nestum447 <ncarpio935@gmail.com>
2025-08-10 08:54:30 -0600

    Create ExcelUpload.js

---
+++ src/components/ExcelUpload.js
@@ -0,0 +1,27 @@
+import React from "react";
+import * as XLSX from "xlsx";
+
+function ExcelUpload({ setData }) {
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const data = new Uint8Array(event.target.result);
+      const workbook = XLSX.read(data, { type: "array" });
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet);
+      setData(jsonData);
+    };
+    reader.readAsArrayBuffer(file);
+  };
+
+  return (
+    <div>
+      <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
+    </div>
+  );
+}
+
+export default ExcelUpload;
